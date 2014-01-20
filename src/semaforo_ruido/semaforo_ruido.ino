fa41687f92b80718c2b66ef900ec56166e40e7c4
Lluis Toyos <toyos@parda.korama.org>
2014-01-20 22:00:08 +0100

    actualizado el texto del GNUJ GPL

--- src/semaforo_ruido/semaforo_ruido.ino
+++ src/semaforo_ruido/semaforo_ruido.ino
@@ -1,20 +1,25 @@
 /*
  semaforo_ruido 
- ---------------
-     This file is part of SEMAFORO_RUIDO.
+  ---------------
+     This file is part of the Project  SEMAFORO_RUIDO OPEN_SOURCE
+     - A Hardware+Software System to detect the noise levels and to sign them
+     through a 4 colored LED system
 
-    SEMAFORO_RUIDO is free software: you can redistribute it and/or modify
+    Copyright (C) 2014 Lluis Toyos
+
+    This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
 
-    SEMAFORO_RUIDO is distributed in the hope that it will be useful,
+    This program is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
     GNU General Public License for more details.
 
     You should have received a copy of the GNU General Public License
-    along with SEMAFORO_RUIDO.  If not, see <http://www.gnu.org/licenses/>.
+    along with this program. If not, see <http://www.gnu.org/licenses/>.
+    
 ---------------    
     Archivo: semaforo_ruido.ino
     Fecha:20-ENE-2014
